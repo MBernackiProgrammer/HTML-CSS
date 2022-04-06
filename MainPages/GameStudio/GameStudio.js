e249b907631d086784ece99b02965874feb7049b
MBernackiProgrammer <101584672+MBernackiProgrammer@users.noreply.github.com>
2022-04-07 00:56:09 +0200

    Enter

--- MainPages/GameStudio/GameStudio.js
+++ MainPages/GameStudio/GameStudio.js
@@ -1,38 +1,11 @@
 var Index = 0;
 
-function ImproveSize()
+function Up()
 {
-    var element = document.getElementById("block");
-    //var element = document.getElementsById("block");
-    document.get
-    
-    var h = innerHeight;
-    var w = innerWidth;
-    
-    var widthReady = String(w);
-    widthReady = widthReady + "px";
-    
-    element.style.height = widthReady;
-    
-
-    var heightReady = String(h);
-    heightReady = heightReady + "px";
-    
-    element.style.height = heightReady;
-    //element.style.width = widthReady; 
-    
-    document.getElementById("block1").style.height = heightReady;
-    document.getElementById("block1").style.width = widthReady;
-
-    
-
-     
+    Index = Index + 1;
 }
 
-function changeWidth(ele, wd) {
-    z.innerHTML = h;
-    ele.style.width = wd;
-    ele.style.height = h;
+function Down()
+{
+    Index = Index - 1;
 }
-
-ImproveSize();
