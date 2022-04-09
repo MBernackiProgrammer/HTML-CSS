4910186645a1a46f9dcc88e33f7627cc5eabab5d
MBernackiProgrammer <101584672+MBernackiProgrammer@users.noreply.github.com>
2022-04-09 19:03:21 +0200

    enter

--- MainPages/GameStudio/GameStudio.js
+++ MainPages/GameStudio/GameStudio.js
@@ -1,11 +1,66 @@
-var Index = 0;
+let Index = 0;
 
 function Up()
 {
     Index = Index + 1;
+    if(Index > 1)
+    {
+        Index = 0;
+    }
+    FindWhereMove();
 }
 
 function Down()
 {
     Index = Index - 1;
+    if(Index < 0)
+    {
+        Index = 1;
+        
+    }
+    FindWhereMove();
 }
+
+function FindWhereMove()
+{
+    
+    alert(Index);
+    switch(Index)
+    {
+        case 0:
+            location.href="#block";
+            break;
+
+        case 1:
+            location.href="#block1";
+            break;
+    }
+}
+
+
+
+function ImproveSize()
+{
+    var element = document.getElementById("block");
+    //var element = document.getElementsById("block");
+    document.get
+    
+    var h = innerHeight;
+    var w = innerWidth;
+    
+    var widthReady = String(w);
+    widthReady = widthReady + "px";
+    
+    element.style.height = widthReady;
+    
+
+    var heightReady = String(h);
+    heightReady = heightReady + "px";
+    
+    element.style.height = heightReady;
+    //element.style.width = widthReady; 
+    
+    document.getElementById("block1").style.height = heightReady;
+    document.getElementById("block1").style.width = widthReady;
+}
+
